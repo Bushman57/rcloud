76bd0eeb1ca6b92ba5f8fbe35b768ee68e0592d4
Bushman57 <savinskamau01@gmail.com>
2023-10-05 01:34:40 +0000

    commited

---
+++ App.R
@@ -0,0 +1,18 @@
+library(dipsaus) #stylizing excel styles
+library(shiny)
+
+ui<-fluidPage()
+server<-function(){
+  
+}
+library(shiny)
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui=ui, server=server)
